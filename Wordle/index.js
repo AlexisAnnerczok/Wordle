371cc15e43643fa4bd958b8ef3d022a9f09d9330
Alexis <alexis.annerczok.pro@gmail.com>
2022-09-24 12:07:28 +0200

    Ajout de Wordle

--- Wordle/index.js
+++ Wordle/index.js
@@ -8,23 +8,26 @@ var input = [];
 // Fonction RandomWord qui génère un mot au hasard
 function RandomWord() {
     random = (0, lib_1.getRandomWord)().split('');
-    console.log(random);
 }
 // Fonction Input qui récupère une entrée utilisateur et vérifie la correspondence des mots
 function Input() {
     input = (0, lib_1.getInput)('Type a word of 5 letters').toUpperCase().split('');
+    if (input.length !== 5) {
+        return Input();
+    }
     console.log(input); // A enlever après
     for (var i = 0; i < random.length; i += 1) {
         if (input[i] !== random[i]) {
             if (random.includes(input[i])) {
-                console.log("The letter ".concat(input[i], " is in the word but in the wrong spot \n"));
+                var l = input[i];
+                console.log("The letter \u001B[33m".concat(input[i], "\u001B[0m is in the word but in the wrong spot \n"));
             }
             else {
                 console.log("The letter ".concat(input[i], " is not in the world in any spot \n"));
             }
         }
         else {
-            console.log("The letter ".concat(input[i], " is in the word and in the correct place \n"));
+            console.log("The letter \u001B[32m".concat(input[i], "\u001B[0m is in the word and in the correct place \n"));
         }
     }
     for (var i2 = 0; i2 < random.length; i2 += 1) {
@@ -36,7 +39,6 @@ function Input() {
 }
 // Lancement d'une partie
 function Play() {
-    console.log('Tapez un mot pour commencer');
     RandomWord();
     Input();
 }
--- Wordle/index.ts
+++ Wordle/index.ts
@@ -8,23 +8,26 @@ let input: string[] = [];
 // Fonction RandomWord qui génère un mot au hasard
 function RandomWord() {
   random = getRandomWord().split('');
-  console.log(random);
 }
 
 // Fonction Input qui récupère une entrée utilisateur et vérifie la correspondence des mots
 
 function Input() {
   input = getInput('Type a word of 5 letters').toUpperCase().split('');
+  if (input.length !== 5){
+    return Input();
+  }
   console.log(input); // A enlever après
   for (let i: number = 0; i < random.length; i += 1) {
     if (input[i] !== random[i]) {
-      if (random.includes(input[i])) {
-        console.log(`The letter ${input[i]} is in the word but in the wrong spot \n`);
+      if (random.includes(input[i])) { 
+        let l = input[i]
+        console.log(`The letter \x1b[33m${input[i]}\x1b[0m is in the word but in the wrong spot \n`);
       } else {
         console.log(`The letter ${input[i]} is not in the world in any spot \n`);
       }
     } else {
-      console.log(`The letter ${input[i]} is in the word and in the correct place \n`);
+      console.log(`The letter \x1b[32m${input[i]}\x1b[0m is in the word and in the correct place \n`);
     }
   }
   for (let i2: number = 0; i2 < random.length; i2 += 1) {
@@ -37,7 +40,6 @@ function Input() {
 
 // Lancement d'une partie
 function Play() {
-  console.log('Tapez un mot pour commencer');
   RandomWord();
   Input();
 }
